9366464c159831fdd4493ee3cb226ebdf9094c3a
PabloAC04 <pabloarquecebri@gmail.com>
2024-05-11 01:10:11 +0200

    Hechos bastantes avances en el código

--- src/prueba.m
+++ src/prueba.m
@@ -1,32 +1,22 @@
-clc; clear; close all;
+clear; close all;
 
 % Carga de datos
 data = readtable('../data/properati_argentina_2021_tp1.csv');
 
-% Convertir property_type a variables dummy y concatenar
+%% Fase de preprocesado de datos
+
+% Convertir property_type a columna por tipo
 data.property_type = categorical(data.property_type);
 dummies = dummyvar(data.property_type);
 dummyTable = array2table(dummies, 'VariableNames', {'Casa', 'Departamento', 'PH'});
-dummyTable.Casa = [];  % Eliminación para evitar multicolinealidad
-
+dummyTable.PH = [];  % Eliminación para evitar multicolinealidad
 data = [data dummyTable];
 
-% Eliminar columnas innecesarias
+% Eliminacion de caracteristicas no necesarias
 data.property_type = [];
-data.pxm2 = []; % Asumiendo que quieres eliminar esta columna
+data.pxm2 = []; 
 data.place_l3 = [];
 data.tipo_precio = [];
-
-% Transformaciones potenciales
-% Escalar latitud y longitud
-data.latitud = data.latitud * 1000;
-data.longitud = data.longitud * 1000;
-
-% Interacción entre algunas variables
-data.Interaccion1 = data.property_rooms .* data.property_bedrooms;
-data.Interaccion2 = data.property_surface_total .* data.property_surface_covered;
-%data.Departamento = [];
-%data.PH = [];
 
 % Definir la variable dependiente y las independientes
 y = data.property_price;
@@ -34,38 +24,125 @@ data.property_price = []; % Eliminar la columna del precio de la tabla
 
 x = data.Variables;
 
-idout1 = find(y>mean(y)*3);
-idout2 = find(y<mean(y)/3);
 
-y([idout1; idout2]) = [];
-x([idout1; idout2],:) = [];
+% Identificación de valores atípicos
+outliers = isoutlier(y, 'quartiles');
+x(outliers, :) = [];
+y(outliers) = [];
 
-% Asegurar que todas las otras columnas son numéricas
-T = size(x, 1);
+% Normalizacion simple de los datos media 0 y varianza 1
 
-% Barajamos los datos
-id = randperm(T);
-x = x(id, :);
-y = y(id);
+x = (x-mean(x)) ./ std(x);
 
-XTrain = x(1:floor(T*0.75)-1,:);
-YTrain = y(1:floor(T*0.75)-1,:);
+% División de datos en validación y prueba
+splitPoint = floor(0.80 * size(x, 1));
+XVal = x(1:splitPoint,:);
+YVal = y(1:splitPoint);
+XTest = x(splitPoint+1:end,:);
+YTest = y(splitPoint+1:end);
 
-XTest = x(floor(T*0.75):end,:);
-YTest = y(floor(T*0.75):end,:);
+%% Validacion
+% Random sampling para ver qué modelo se ajusta mejor
+numReps = 3;
+Ers = zeros(6,1);
 
-% Entrenar un modelo de regresión lineal
-mdl = fitlm(XTrain, YTrain);
+for order=1:10
+    error = zeros(numReps,1);
+    for k=1:numReps
+        % Muestreo aleatorio de los datos de validación
+        ids = randperm(size(XVal, 1));
+        midPoint = floor(0.75 * length(ids));
+        xtrn = XVal(ids(1:midPoint), :);
+        ytrn = YVal(ids(1:midPoint));
+        xtst = XVal(ids(midPoint+1:end), :);
+        ytst = YVal(ids(midPoint+1:end));
+        
+        % Añadir términos polinomiales
+        for j = 2:order
+            xtrn = [xtrn XVal(ids(1:midPoint),:).^j];
+            xtst = [xtst XVal(ids(midPoint+1:end),:).^j];
+        end
+        
+        % Modelo de regresión
+        A = [xtrn, ones(size(xtrn, 1), 1)];
+        sol = pinv(A) * ytrn;
+        ATest = [xtst, ones(size(xtst, 1), 1)];
+        ypred = ATest * sol;
+        error(k) = mean(abs(ytst - ypred));  % RMSE
+    end
+    Ers(order) = mean(error);
+end
 
-% Evaluar el modelo
-YPred = predict(mdl, XTest);
-rmse = sqrt(mean((YPred - YTest).^2));
+bar(Ers);
+xlabel('Order of Polynomial');
+ylabel('Average RMSE');
+title('Model Complexity vs. Prediction Error');
 
-disp(['RMSE del modelo: ', num2str(rmse)]);
+pause;
 
-% Inspeccionar los coeficientes
-disp(mdl.Coefficients);
+[~,orden] = min(Ers);
 
+disp("El polinomio que mejor se ajusta es el de orden "+orden);
+
+%% Seleccion de caracteristica por pasos
+XTrain = XVal;
+YTrain = YVal;
+Xaux = XTest;
+
+selected_features = false(1, size(XTrain, 2));
+model_features = [];
+best_rmse = inf;
+
+for i=2:orden
+    XTrain = [XTrain XVal.^i];
+    XTest = [XTest XTest.^i];
+end
+
+for i = 1:size(XTrain, 2)
+    feature_rmse = inf(1, size(XTrain, 2));
+
+    for j = find(~selected_features)
+        features = [model_features j];
+
+        % Añadir una columna de unos para el término intercepto
+        A = [XTrain(:, features) ones(size(XTrain, 1), 1)];
+        sol = pinv(A) * YTrain;
+        YPredTrain = A * sol;
+        feature_rmse(j) = mean(abs(YPredTrain - YTrain));
+    end
+
+    [min_rmse, best_feature] = min(feature_rmse);
+    if min_rmse < best_rmse-200
+        best_rmse = min_rmse;
+        model_features = [model_features best_feature];
+        selected_features(best_feature) = true;
+        disp(['Adding feature ', num2str(best_feature), ' with RMSE: ', num2str(best_rmse)]);
+    else
+        break;
+    end
+end
+
+
+% Modelo final con las características seleccionadas
+A = [XTrain(:,features) ones(size(XTrain, 1), 1)];
+sol = pinv(A) * YTrain;
+YPredTrain = A * sol;
+final_rmse = mean(abs(YPredTrain - YTrain));
+disp(['Final RMSE with selected features: ', num2str(final_rmse)]);
+
+% Predicciones sobre el conjunto de prueba
+ATest = [XTest(:,features) ones(size(XTest, 1), 1)];
+YPred = ATest * sol;
+test_rmse = mean(abs(YPred - YTest));
+disp(['Test RMSE with selected features: ', num2str(test_rmse)]);
+
+% Visualización de los resultados
 figure(1);
-plot(1:sizee, YTest, 'r*'); hold on;
-plot(1:19661, YPred, 'b*'); hold off;
+plot(YTest, 'b.'); hold on;
+plot(YPred, 'r.'); 
+legend('Datos reales', 'Predicciones');
+xlabel('Número de muestra');
+ylabel('Precio de la Propiedad');
+title('Comparación de datos reales y predicciones');
+hold off;
+
