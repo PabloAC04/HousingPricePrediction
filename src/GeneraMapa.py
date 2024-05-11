7ea1efd6a1bddbec1d772c9a7c0456de3aa8d68c
PabloAC04 <pabloarquecebri@gmail.com>
2024-05-11 16:31:59 +0200

    Añadida leyenda

--- src/GeneraMapa.py
+++ src/GeneraMapa.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import contextily as ctx
-from PIL import Image
 
 # Cargar datos desde un archivo CSV
 df = pd.read_csv('../data/properati_argentina_2021_tp1.csv', usecols=['latitud', 'longitud', 'tipo_precio'])
@@ -20,7 +20,7 @@ df['color'] = df['tipo_precio'].map(color_map)
 fig, ax = plt.subplots(figsize=(10, 10))
 
 # Plotear puntos usando las coordenadas de longitud y latitud
-ax.scatter(df['longitud'].astype(float), df['latitud'].astype(float), c=df['color'], s=2, alpha=0.5)  # Usar la columna 'color' para los colores
+sc = ax.scatter(df['longitud'].astype(float), df['latitud'].astype(float), c=df['color'], s=2, alpha=0.5)
 
 # Añadir el mapa base
 ctx.add_basemap(ax, crs="EPSG:4326", source=ctx.providers.CartoDB.Positron)
@@ -28,6 +28,10 @@ ctx.add_basemap(ax, crs="EPSG:4326", source=ctx.providers.CartoDB.Positron)
 # Ajustar los límites para centrar los puntos en el mapa
 ax.set_xlim(df['longitud'].min() - 0.05, df['longitud'].max() + 0.05)
 ax.set_ylim(df['latitud'].min() - 0.05, df['latitud'].max() + 0.05)
+
+# Crear handles para la leyenda
+handles = [mpatches.Patch(color=color, label=label) for label, color in color_map.items()]
+ax.legend(handles=handles, title="Tipo de precio", title_fontsize='13', loc='upper right')
 
 # Opcionales: personalizar más el gráfico
 ax.axis('off')  # Ocultar los ejes
